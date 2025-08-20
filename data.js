0a3bf33835921d18a46b8101659e574813db867c
basketballdb <stefanlukjanovic@gmail.com>
2025-08-20 13:11:51 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+const games = [
+  {
+    id: 1,
+    date: "2025-08-15",
+    year: 2025,
+    teamHome: "Poland",
+    teamAway: "Finland",
+    score: "78 - 70",
+    link: "games/game2.html"
+  },
+  {
+    id: 2,
+    date: "2025-08-17",
+    year: 2025,
+    teamHome: "Slovenia",
+    teamAway: "Great Britain",
+    score: "85 - 81",
+    link: "games/game3.html"
+  },
+  {
+    id: 3,
+    date: "2024-07-12",
+    year: 2024,
+    teamHome: "Latvia",
+    teamAway: "Estonia",
+    score: "90 - 75",
+    link: "games/game4.html"
+  },
+  {
+    id: 4,
+    date: "2023-06-20",
+    year: 2023,
+    teamHome: "Germany",
+    teamAway: "France",
+    score: "82 - 79",
+    link: "games/game5.html"
+  }
+]
